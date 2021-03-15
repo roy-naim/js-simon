d259f1d02668e570defb93d5f6c9f0e61415b9c9
Roy <b.roynaim@gmail.com>
2021-03-15 16:47:19 +0100

    printed number

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
 
-var array = [];
-var max = 100;
-// FUNCTIONS
-function createBombs(array, max) {
-  while (array.length <=5 -1) {
-    var number = randomNumberInRange(1, max)
-
-    if (!array.includes(number) == true) {
-      array.push(number);
-    }
-  }
-  return array;
+var arr = [];
+while(arr.length < 5){
+    var r = Math.floor(Math.random() * 100) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
 }
 
-function randomNumberInRange(min, max) {
-  if (isNaN(min) || isNaN(max)) {
-    console.log("Non sono numeri");
-  } else {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-}
-
-
-document.getElementById('numbers').textContent = createBombs();
+document.getElementById('numbers').textContent = arr;
