6f3463209e6f10b73d19f03a0fb519ae6868f8cd
Roy <b.roynaim@gmail.com>
2021-03-15 16:43:44 +0100

    printing numbers

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
 
+var array = [];
+var max = 100;
 // FUNCTIONS
-function randomNumber(min, max) {
-  var numero = Math.floor(Math.random() * (max) + min);
-  return numero;
+function createBombs(array, max) {
+  while (array.length <=5 -1) {
+    var number = randomNumberInRange(1, max)
 
-  document.getElementById('numbers');
-
+    if (!array.includes(number) == true) {
+      array.push(number);
+    }
+  }
+  return array;
 }
+
+function randomNumberInRange(min, max) {
+  if (isNaN(min) || isNaN(max)) {
+    console.log("Non sono numeri");
+  } else {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+}
+
+
+document.getElementById('numbers').textContent = createBombs();
